57cd20cc9319b582ed655ca60ae5b2de65f15fa9
Francesco Di Salvo <francesco.disalvo99@gmail.com>
2021-09-10 08:49:58 +0200

    Added parser arguments

--- voc_inversion.py
+++ voc_inversion.py
@@ -69,62 +69,21 @@ def run(args):
         net = build_resnet("resnet34", "classic")
     else:
         print("loading torchvision model for inversion with the name: {}".format(args.arch_name))
-        # this is the teacher
-        # so we need to upload here the pre trained arch on the VOC
-        # net = models.__dict__["resnet50"](pretrained=False, num_classes=16)
 
-        checkpoint_teacher = torch.load("/content/drive/MyDrive/step-0-resnet50.pth")
-        # checkpoint_teacher_v2 = {}
-        # print(checkpoint_teacher)
+        checkpoint_teacher = torch.load(args.teacher_path)
         checkpoint_teacher = checkpoint_teacher['model_state']
 
         head = BiSeNet("resnet50")
         body = "resnet50"
-        classes_edit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
         net = IncrementalSegmentationBiSeNet(body, head, classes=[16], fusion_mode="mean")
-
-        # print(net)
-        # net.supervision1[0] = nn.Conv2d(in_channels=1024, out_channels=16, kernel_size=1)
-        # net.supervision2[0] = nn.Conv2d(in_channels=2048, out_channels=16, kernel_size=1)
-        # net.cls[0] = nn.Conv2d(in_channels=256, out_channels=16, kernel_size=1)
-
-        # print(net)
 
         net.load_state_dict(checkpoint_teacher, strict=False)
 
-        """
-        for k, v in checkpoint_teacher.items():
-            # we're interested into : head.context_path
-
-            if k.split(".")[1] == "context_path" \
-                    and k.split(".")[2] not in ["features"]:
-                new_k = k.replace("head.context_path.", "")
-                checkpoint_teacher_v2[new_k] = v
-
-            # edge cases -> we moved out the fc layers for BiSeNet, so
-            # they are called on our checkpoint "cls"
-            if k.split(".")[0] == "cls":
-
-                if k.split(".")[2] == "bias":
-                    checkpoint_teacher_v2["fc.bias"] = v
-                elif k.split(".")[2] == "weight":
-                    checkpoint_teacher_v2["fc.weight"] = v
-
-        net.fc = nn.Conv2d(in_channels=256, out_channels=16, kernel_size=1)
-        net.load_state_dict(checkpoint_teacher_v2)
-        print(net)
-        net.layer4[2].bn3 = nn.BatchNorm2d(4096, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-        """
 
     net = net.to(device)
 
-    use_fp16 = args.fp16
-
-
     print('==> Resuming from checkpoint..')
 
-    ### load models
-    # it it the checkpoint of the whole deepinversion model(?)
     if args.arch_name == "resnet50v15":
         path_to_model = "./models/resnet50v15/model_best.pth.tar"
         load_model_pytorch(net, path_to_model, gpu_n=torch.cuda.current_device())
@@ -143,57 +102,21 @@ def run(args):
             net_verifier.eval()
 
 
-    # since there is competiton among teacher and student
-    # the verifier will be the actual student
     if args.adi_scale != 0.0:
         student_arch = "resnet18"
-        # here we should load our pre trained network on the VOC
 
-        """
-        net_verifier = models.__dict__[student_arch](pretrained=False, num_classes=16).to(device)
-        net_verifier.eval()
+        checkpoint_verifier = torch.load(args.student_path)
 
-        checkpoint_ver = torch.load("/content/drive/MyDrive/step-0-resnet18.pth")['model_state']
-        checkpoint_ver_v2 = {}
-
-        for k, v in checkpoint_ver.items():
-            # we're interested into : head.context_path
-
-            if k.split(".")[1] == "context_path" and k.split(".")[2] != "features":
-                new_k = k.replace("head.context_path.", "")
-                checkpoint_ver_v2[new_k] = v
-
-            # edge cases -> we moved out the fc layers for BiSeNet, so
-            # they are called on our checkpoint "cls"
-            if k.split(".")[0] == "cls":
-                if k.split(".")[2] == "bias":
-                    checkpoint_ver_v2["fc.bias"] = v
-                elif k.split(".")[2] == "weight":
-                    checkpoint_ver_v2["fc.weight"] = v
-        """
-
-        checkpoint_verifier = torch.load("/content/drive/MyDrive/step-0-resnet18.pth")
-        # checkpoint_teacher_v2 = {}
-        # print(checkpoint_teacher)
         checkpoint_verifier = checkpoint_verifier['model_state']
         head = BiSeNet("resnet18")
         body = "resnet18"
-        classes_edit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
         net_verifier = IncrementalSegmentationBiSeNet(body, head, classes=[16], fusion_mode="mean")
-
         net_verifier.load_state_dict(checkpoint_verifier, strict=False)
-
-        # checkpoint_ver_v2['fc.weight'] = checkpoint_ver_v2['fc.weight'][:, :, 0, 0]
-        # net_verifier.fc = nn.Conv2d(in_channels=256, out_channels=16, kernel_size=1)
-        # net_verifier.load_state_dict(checkpoint_ver_v2)
 
         net_verifier = net_verifier.to(device)
         net_verifier.train()
 
-        """if use_fp16:
-            for module in net_verifier.modules():
-                if isinstance(module, nn.BatchNorm2d):
-                    module.eval().half()"""
 
     from deepinversion import DeepInversionClass
 
@@ -205,12 +128,12 @@ def run(args):
 
     args.iterations = 2000
     args.start_noise = True
-    # args.detach_student = False
 
     args.resolution = 224
     bs = args.bs
     jitter = 30
 
+    # default settings
     parameters = dict()
     parameters["resolution"] = 256
     parameters["random_label"] = False
@@ -236,7 +159,6 @@ def run(args):
 
     network_output_function = lambda x: x
 
-    # check accuracy of verifier
     if args.verifier:
         hook_for_display = lambda x, y: validate_one(x, y, net_verifier)
     else:
@@ -262,6 +184,7 @@ def run(args):
 
 def main():
     parser = argparse.ArgumentParser()
+
     parser.add_argument('-nb', '--n_batches', type=int, default=1, help='Number of batches to generate for each run')
     parser.add_argument('-s', '--worldsize', type=int, default=1, help='Number of processes participating in the job.')
     parser.add_argument('--local_rank', '--rank', type=int, default=0, help='Rank of the current process.')
@@ -296,6 +219,15 @@ def main():
     parser.add_argument('--main_loss_multiplier', type=float, default=1.0,
                         help='coefficient for the main loss in optimization')
     parser.add_argument('--store_best_images', action='store_true', help='save best images as separate files')
+
+    # new arguments defined us for the sake of simplicity
+    # >> default teacher : BiSeNet with a ResNet50 as backbone
+    # >> default student : BiSeNet with a ResNet18 as backbone
+    parser.add_argument('--teacher_path', type=str, default="/content/drive/MyDrive/step-0-resnet50.pth",
+                        help="Path of the pre trained teacher path")
+    parser.add_argument('--student_path', type=str, default="/content/drive/MyDrive/step-0-resnet18.pth",
+                        help="Path of the pre trained student path")
+
 
     args = parser.parse_args()
     print(args)
